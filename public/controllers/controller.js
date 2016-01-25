b3421d19cbfa4f97e4865dbf659898cfdf54ee48
67mohamed <67mohamed@gmail.com>
2016-01-25 16:53:45 -0500

    Rough Draft

---
+++ public/controllers/controller.js
@@ -0,0 +1,44 @@
+var myApp = angular.module('myApp',[]);
+myApp.controller('AppCtrl',['$scope','$http',
+	function($scope, $http){
+
+		//Refreshes the contact UI to correctly represent in the information in the database.
+		//This is called whenever an operation is done that changes the data in the database.
+		var refresh = function(){
+			$http.get("/contactlist").success(function(response){
+				$scope.contactlist = response;
+				$scope.task = '';
+			});
+		};
+
+		refresh();
+
+		//Sends a post request to the database when the user adds a contact.
+		$scope.addContact = function(){
+			console.log($scope.task);
+
+			$http.post('/addToArray', $scope.task).success(function(response){
+				refresh();
+			});
+
+		};
+
+		$scope.sendMsg = function(){
+			console.log($scope.task);
+
+			$http.post('/sendMsg', $scope.task).success(function(response){
+				refresh();
+			});
+
+		};
+
+		//Removes an element from the database based on a given id. 
+		//Refreshes the page to correctly represent the data in the database.
+		$scope.remove = function(name){
+			$http.delete('/contactlist/' + name).success(function(response){
+				refresh();
+			});
+
+		};
+
+}]);
---
+++ server.js
@@ -0,0 +1,116 @@
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist',['contactlist']);
+var bodyParser = require('body-parser');
+var cron = require('cron');
+
+// Twilio Credentials 
+var accountSid = 'AC3a0f1edd1cea484b5dde987eef124dc3'; 
+var authToken = '7c60118184d4b86ac960edbe87227ab6'; 
+
+//require the Twilio module and create a REST client 
+var client = require('twilio')(accountSid, authToken); 
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+//Retrieves all entries from the database.
+app.get('/contactlist', function(req, res){
+	db.contactlist.find(function(err, docs){
+		res.send(array);
+	});
+});
+
+//Adds an entry to the database
+app.post('/contactlist', function(req, res){
+	console.log(req.body.name);
+	db.contactlist.insert(req.body, function(err, doc){
+		res.json(doc);
+	});
+});
+
+var array = ["Always","First","Wow","maybe"];
+
+var sendMsg = function(message){
+	
+	
+	client.messages.create({ 
+	to: "+13174183511", 
+	from: "+13473219669", 
+	body: message,   
+}, function(err, message) { 
+	console.log(message.sid); 
+});
+  
+
+};
+
+var sendMsg2 = function(message){
+	console.log(message);
+}
+
+
+
+var cronJob = cron.job("*/5 * * * * *", function(){
+    // perform operation e.g. GET request http.get() etc.
+    console.log('cron job completed');
+}); 
+cronJob.start();
+
+/*
+function doSomething() {}
+
+(function loop() {
+    var rand = Math.round(Math.random() * 6000);
+    setTimeout(function() {
+            //alert('A');
+            sendMsg2(array[Math.round(Math.random()*array.length)-1]);
+            loop();  
+    }, rand);
+}());
+*/
+
+//Adds an entry to the array
+app.post('/addToArray', function(req, res){
+	
+	array.push(req.body.name);
+	console.log(array);
+	res.send(array);
+});
+
+app.post('/sendMsg', function(req, res){
+	console.log(req.body.name);
+	sendMsg(req.body.name);
+	res.send(array);
+});
+
+//Deletes an entry from the database based on the given id.
+app.delete('/contactlist/:id', function(req, res){
+	var id = req.params.id;
+	db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc){
+		res.json(doc);
+	});
+});
+
+//Returns information that's related to a given id.
+app.get('/contactlist/:id', function(req, res){
+	var id = req.params.id;
+	db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc){
+		res.json(doc);
+	});
+});
+
+//Used to edit an existing entry
+app.put('/contactlist/:id', function(req, res){
+	var id = req.params.id;
+	db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+		update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+		new: true}, function(err, doc){
+			res.json(doc);
+		
+	});
+});
+
+app.listen(3000);
+console.log("Server running on port 3000");
